b36cde3548f256a479122a914c30a374fed33a5d
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-10-02 20:51:23 +0700

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,10 @@ const { StatusCodes } = require("http-status-codes");
 const logger = require("../config/logger");
 const auth_function = require("../functions/auth");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 exports.register = async (req, res) => {
   try {
@@ -42,7 +45,57 @@ exports.register = async (req, res) => {
       message: "Success create user",
     });
   } catch (error) {
-    logger.error(error);
+    logger.error("auth/register error: " + error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { error } = await auth_function.validateLogin(req.body);
+    if (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: error.details[0].message,
+      });
+    }
+
+    const user = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (!user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: "Email or password wrong",
+      });
+    }
+
+    const matchPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!matchPassword) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: "Email or password wrong",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    delete user.dataValues.password;
+    delete user.dataValues.createdAt;
+    delete user.dataValues.updatedAt;
+
+    res.status(StatusCodes.OK).json({
+      message: "Success login",
+      data: { ...user.dataValues, token },
+    });
+  } catch (error) {
+    logger.error("auth/login error: " + error);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: error.message,
     });
