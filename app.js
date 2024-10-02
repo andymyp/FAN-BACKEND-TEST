411540ecb34b8b094853bbe4608c07f2f3541ef1
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-10-02 16:45:16 +0700

    setup express

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//! Middlewares
+app.use(cors());
+app.use(express.json());
+
+//! Public Routes
+app.use("/status", (req, res) => {
+  res.status(200).json({
+    status: 1,
+    message: "Server is running",
+  });
+});
+
+//! Not Found
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Endpoint was not found" });
+});
+
+//! Connection
+const APP_PORT = process.env.APP_PORT || 3000;
+
+app.listen(APP_PORT, () => {
+  console.log("App running in port:", APP_PORT);
+});
