6d82f07cb94b06337dc88adb927869024fa67a77
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-10-02 23:21:51 +0700

    get user epresences

--- controllers/epresence.js
+++ controllers/epresence.js
@@ -4,6 +4,7 @@ const epresence_function = require("../functions/epresence");
 const Epresence = require("../models/epresence");
 const { Op } = require("sequelize");
 const moment = require("moment-timezone");
+const User = require("../models/user");
 
 exports.createEpresence = async (req, res) => {
   try {
@@ -16,9 +17,6 @@ exports.createEpresence = async (req, res) => {
 
     const userId = req.user?.id;
     const userSupervisor = req.user?.npp_supervisor;
-
-    console.log("userId", userId);
-    console.log("userSupervisor", userSupervisor);
 
     if (!userId || userSupervisor === "" || userSupervisor === null) {
       return res.status(StatusCodes.UNAUTHORIZED).json({
@@ -61,6 +59,62 @@ exports.createEpresence = async (req, res) => {
     });
   } catch (error) {
     logger.error("epresence/createEpresence error: " + error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.getEpresences = async (req, res) => {
+  try {
+    const userId = req.user?.id;
+
+    const epresences = await Epresence.findAll({
+      where: { userId },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const groupedRecords = {};
+
+    epresences.forEach((epresence) => {
+      const date = moment(epresence.time).format("YYYY-MM-DD");
+
+      if (!groupedRecords[date]) {
+        groupedRecords[date] = {
+          userId: epresence.userId,
+          userName: epresence.User.name,
+          date,
+          inTime: null,
+          outTime: null,
+          inStatus: null,
+          outStatus: null,
+        };
+      }
+
+      if (epresence.type === "IN") {
+        groupedRecords[date].inTime = moment(epresence.time).format("HH:mm:ss");
+        groupedRecords[date].inStatus =
+          epresence.isApprove === "TRUE" ? "APPROVE" : "REJECT";
+      } else if (epresence.type === "OUT") {
+        groupedRecords[date].outTime = moment(epresence.time).format(
+          "HH:mm:ss"
+        );
+        groupedRecords[date].outStatus =
+          epresence.isApprove === "TRUE" ? "APPROVE" : "REJECT";
+      }
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      message: "Success get epresence",
+      data: Object.values(groupedRecords),
+    });
+  } catch (error) {
+    logger.error("epresence/getEpresences error: " + error);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: error.message,
     });
--- routes/epresence.js
+++ routes/epresence.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/epresence");
 
 router.post("/epresence", controller.createEpresence);
+router.get("/epresence", controller.getEpresences);
 
 module.exports = router;
